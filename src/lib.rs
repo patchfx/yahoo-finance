d519abbc6558616c1b511d7f004aad34007ca835
Richard Patching <richard@justaddpixels.com>
2017-08-24 16:51:46 +0100

    Get latest price for a stock

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,40 @@
+extern crate curl;
+
+use curl::easy::Easy;
+
+const YAHOO_FINANCE_URL: &'static str = "http://download.finance.yahoo.com/d/quotes.csv?s=";
+
+pub struct Stock {
+    symbol: String,
+    current: String,
+}
+
+impl Stock {
+    pub fn update_current_price(&mut self) {
+      let mut data = Vec::new();
+      let mut curl = Easy::new();
+      let url = format!("{}{}&f=l1", YAHOO_FINANCE_URL, self.symbol);
+      curl.url(&url).unwrap();
+      {
+        let mut transfer = curl.transfer();
+        transfer.write_function(|d| {
+          data.extend_from_slice(d);
+          Ok(d.len())
+        }).unwrap();
+        transfer.perform().unwrap()
+      }
+      self.current = String::from_utf8(data).unwrap();
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
+    fn can_update_a_stocks_current_price() {
+      let mut stock = Stock { symbol: "IBM".to_string(), current: "0.0".to_string() };
+      stock.update_current_price();
+      assert!(stock.current != "0.0");
     }
 }
