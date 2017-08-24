90b1595de0b365964c4213ee8faa904633562986
Richard Patching <richard@justaddpixels.com>
2017-08-24 17:06:02 +0100

    Add yahoo_finance namespace

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,31 @@
 extern crate curl;
 
-use curl::easy::Easy;
+mod yahoo_finance {
+  const YAHOO_FINANCE_URL: &'static str = "http://download.finance.yahoo.com/d/quotes.csv?s=";
+  use curl::easy::Easy;
 
-const YAHOO_FINANCE_URL: &'static str = "http://download.finance.yahoo.com/d/quotes.csv?s=";
+  pub struct Stock {
+      pub symbol: String,
+      pub current: String,
+  }
 
-pub struct Stock {
-    symbol: String,
-    current: String,
-}
-
-impl Stock {
-    pub fn update_current_price(&mut self) {
-      let mut data = Vec::new();
-      let mut curl = Easy::new();
-      let url = format!("{}{}&f=l1", YAHOO_FINANCE_URL, self.symbol);
-      curl.url(&url).unwrap();
-      {
-        let mut transfer = curl.transfer();
-        transfer.write_function(|d| {
-          data.extend_from_slice(d);
-          Ok(d.len())
-        }).unwrap();
-        transfer.perform().unwrap()
+  impl Stock {
+      pub fn update_current_price(&mut self) {
+        let mut data = Vec::new();
+        let mut curl = Easy::new();
+        let url = format!("{}{}&f=l1", YAHOO_FINANCE_URL, self.symbol);
+        curl.url(&url).unwrap();
+        {
+          let mut transfer = curl.transfer();
+          transfer.write_function(|d| {
+            data.extend_from_slice(d);
+            Ok(d.len())
+          }).unwrap();
+          transfer.perform().unwrap()
+        }
+        self.current = String::from_utf8(data).unwrap();
       }
-      self.current = String::from_utf8(data).unwrap();
-    }
+  }
 }
 
 #[cfg(test)]
@@ -33,7 +34,7 @@ mod tests {
 
     #[test]
     fn can_update_a_stocks_current_price() {
-      let mut stock = Stock { symbol: "IBM".to_string(), current: "0.0".to_string() };
+      let mut stock = yahoo_finance::Stock { symbol: "IBM".to_string(), current: "0.0".to_string() };
       stock.update_current_price();
       assert!(stock.current != "0.0");
     }
